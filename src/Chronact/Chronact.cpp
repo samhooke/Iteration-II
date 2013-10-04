7ff36f10f5c2056ea91c0c67f725c67722cbd18c
Sam Hooke <marbsam@yahoo.co.uk>
2013-10-04 10:18:42 +0100

    Improved title

--- src/Chronact/Chronact.cpp
+++ src/Chronact/Chronact.cpp
@@ -110,7 +110,7 @@ void Chronact::RoomTitle(RoomEvent roomEvent) {
     case RoomEvent::Update:
         timeSinceLastDraw += deltaTime.asSeconds();
         if (timeSinceLastDraw > (float)1/60) {
-            display->DrawBackground();
+            //display->DrawBackground();
             display->DrawTitle();
             timeSinceLastDraw = 0;
         }
