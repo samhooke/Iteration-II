84d3a5abb488f6a73007e73a3d257f8d5b575560
Sam Hooke <marbsam@yahoo.co.uk>
2013-10-08 12:44:18 +0100

    Restored main try/catch loop
    Also fixed the #include that was pointing to the old Chronact directory.

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,18 +1,15 @@
-#include "Chronact/Chronact.hpp"
+#include "Chronact.hpp"
 #include <iostream>
 
 int main() {
 
     Chronact* chronact = new Chronact();
-    chronact->Go();
 
-    /*
     try {
         chronact->Go();
     } catch (std::exception& e) {
         std::cout << e.what();
     }
-    */
 
     delete chronact;
 }
