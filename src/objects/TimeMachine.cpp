d72ec6e3548882791584b4d125bc8769dc398dd8
Sam Hooke <marbsam@yahoo.co.uk>
2013-10-10 18:01:26 +0100

    Player reappears in the past and gains control

--- src/objects/TimeMachine.cpp
+++ src/objects/TimeMachine.cpp
@@ -14,9 +14,15 @@ namespace GameObject {
         if (Controlling()) {
             if (game->gameClock->getElapsedTime().asSeconds() > lastActionTime + MOVEMENT_DELAY) {
                 bool key_action = sf::Keyboard::isKeyPressed(sf::Keyboard::Z);
+                bool key_action2 = sf::Keyboard::isKeyPressed(sf::Keyboard::X);
                 if (key_action) {
+                    // Go back in time
                     lastActionTime = game->gameClock->getElapsedTime().asSeconds();
                     levelManager->iterationData->Backward();
+                } else if (key_action2) {
+                    // Exit time machine (create new player and pass control to them)
+                    levelManager->levelData->CreatePlayer(x, y, true);
+                    hasControl = false;
                 }
             }
         }
