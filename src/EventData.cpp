410d2f864f9ae335496c5081a05295860d94970b
Sam Hooke <marbsam@yahoo.co.uk>
2013-10-13 21:19:00 +0100

    All events are destroyed appropriately

--- src/EventData.cpp
+++ src/EventData.cpp
@@ -3,7 +3,14 @@
 #include <iostream> // Temporary
 
 EventData::EventData() {}
-EventData::~EventData() {}
+EventData::~EventData() {
+    // Destroy all events
+    for (std::map<int, std::vector<Event::Base*>>::iterator itr = events.begin(), itrEnd = events.end(); itr != itrEnd; ++itr) {
+        for (int i = 0; i < (itr->second).size(); ++i) {
+            delete (itr->second).at(i);
+        }
+    }
+}
 
 void EventData::AddEvent(Event::Base* event) {
     // Read the time from the event and use that as the key
@@ -13,14 +20,14 @@ void EventData::AddEvent(Event::Base* event) {
 
 void EventData::ExecuteForwardEvents(int time) {
     // Execute events in forwards order
-    for (int i = 0; i < (int)events[time].size(); i++) {
+    for (int i = 0; i < (int)events[time].size(); ++i) {
         events[time].at(i)->ForwardEvent();
     }
 }
 
 void EventData::ExecuteBackwardEvents(int time) {
     // Execute events in backwards order
-    for (int i = (int)events[time].size() - 1; i >= 0 ; i--) {
+    for (int i = (int)events[time].size() - 1; i >= 0 ; --i) {
         events[time].at(i)->BackwardEvent();
     }
 }
